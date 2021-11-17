a09b2c4ba60482e7df15f45707c2d6eeb443f4e0
yaoshuai <yaoshuai@hortorgames.com>
2021-11-17 20:29:34 +0800

    [工具]修改了build

--- build.sh
+++ build.sh
@@ -5,7 +5,9 @@ src="./"
 dest="./build"
 rm -rf $dest
 mkdir -p $dest/scripts/
+mkdir -p $dest/templates/
 cp ./scripts/* ./build/scripts/
+cp ./templates/* ./build/templates/
 GOOS=linux GOARCH=amd64 go build -o $dest/$proj ./main/main.go
 cur_dateTime="`date +%Y%m%d%H%M`"
 cd $dest && tar -czvf sample_web_$cur_dateTime.tar.gz ./*
